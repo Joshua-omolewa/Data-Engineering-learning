f3ecdc0ed74798319d7bf0f18cb1a3c0223a83f7
Joshua-omolewa <omolewa@ualberta.ca>
2023-07-30 18:48:32 -0600

    Working on bash script, uploading and modifying bash script

---
+++ Bash-scripting/task3/array_value.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# print all the colors in the array below
+
+declare -a colors
+
+colors=(yellow red blue green black white purple pink)
+
+# write your solution below
---
+++ Bash-scripting/task3/array_value_joshua.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Created by Joshua Omolewa
+
+# print all the colors in the array below
+
+declare -a colors
+
+colors=(yellow red blue green black white purple pink)
+
+# write your solution below
+
+echo "${colors[@]}"
